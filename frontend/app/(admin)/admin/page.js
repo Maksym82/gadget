f305ffc4cab3a6cfb0891df4e65681b34dba66f7
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-07-19 20:31:47 +0300

    Update

--- frontend/app/(admin)/admin/page.js
+++ frontend/app/(admin)/admin/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import { UserIcon } from "@heroicons/react/24/outline";
 
-export default function Home() {
+export default function Auth() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="text-2xl">Hello Next!!!</div>
-      </main>
+    <div className="w-full h-screen flex items-center justify-center dark:bg-gray-800 bg-gray-200">
+      <div>
+        <UserIcon className="size-6 text-blue-500" />
+        Hello Next!!!
+      </div>
     </div>
   );
 }
