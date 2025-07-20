141e44560f9006b6b9bd940ae022e23723751fe4
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-07-20 12:31:39 +0300

    Update page.js

---
+++ frontend/app/(admin)/admin/page.js
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center dark:bg-gray-800 bg-gray-200">
+      <div className="dark:bg-gray-700 bg-white p-8 rounded-lg shadow-lg w-96">
+        <div className="flex items-center mb-4 gap-2 justify-center mb-6">
+          Dashboard page content goes here.
+        </div>
+      </div>
+    </div>
+  );
+}
